5eec7107cb492b77e904b6b2852de633d8cc2e07
Giap Hiep <hiep.giapvan@gmail.com>
2022-06-21 09:21:27 +0700

    first init

---
+++ js/main.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+    
+    new WOW().init();
+
+    $(document).on('click', '.qa-item', function(e) {
+        let _this = $(this)
+        
+        if(_this.find('.qa-content').is(':visible')) {
+            _this.find('.qa-content').slideToggle(function() {
+                _this.find('.qa-head').css('border-bottom-left-radius', '10px')
+                _this.find('.qa-head').css('border-bottom-right-radius', '10px')
+            })
+            _this.find('img').attr('src', 'images/qa-down.png')
+        } else { 
+            _this.find('.qa-content').slideToggle()
+            _this.find('.qa-head').css('border-bottom-left-radius', 0)
+            _this.find('.qa-head').css('border-bottom-right-radius', 0)
+            _this.find('img').attr('src', 'images/qa-up.png')
+          
+        }
+    })
+    $(document).on('click', '#header .bar', function(e) {
+        if ($('#header .menu').is(':visible')) {
+            $('#header .menu').slideToggle()
+            $(this).html('<span></span><span></span><span></span>').removeClass('close')
+        } else {
+            $('#header .menu').slideToggle()
+            $(this).html('&times;').addClass('close')
+        }
+    })
+
+    // $(document).on('click touchstart', function (e) {
+    //     if ($(e.target).closest("#header .menu").length === 0 && $(e.target).closest("#header .menu").length === 0) {
+    //         $("#header .menu").css('display', 'none');
+    //     }
+    // })
+
+}
